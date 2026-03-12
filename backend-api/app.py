67a9259aab7cd79d852c9aeb70e975f2d0c5a94c
nithin77777 <55943967+nithin77777@users.noreply.github.com>
2026-03-12 12:35:35 +0530

    used file [data.csv] for data storage

--- backend-api/app.py
+++ backend-api/app.py
@@ -2,7 +2,7 @@ import fastapi
 import uvicorn
 import os
 import dotenv
-
+import csv
 
 dotenv.load_dotenv()
 
@@ -16,5 +16,18 @@ def index():
         "status":"Success"
     }
 
+def create_file():
+    i=1
+    with open("../data.csv", mode='w',newline='\n') as f:
+        writer = csv.writer(f,delimiter=',')
+        writer.writerow(["hello","world"])
+        # f.write("hello,world\n")
+        f.close()
+
+        
+    
+
+        
 if __name__=="__main__":
     uvicorn.run("app:app", port=port, host=host)
+    create_file()
